a3c478972a42204c461f436df1a8036ece60a3a3
Eternatus <120200880+Eternarus@users.noreply.github.com>
2023-01-08 16:51:09 +0500

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,27 @@
+const slider = document.querySelector('.block2_section_lower_slider');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', e => {
+  isDown = true;
+  slider.classList.add('block2_section_lower_slider_active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', _ => {
+  isDown = false;
+  slider.classList.remove('block2_section_lower_slider_active');
+});
+slider.addEventListener('mouseup', _ => {
+  isDown = false;
+  slider.classList.remove('block2_section_lower_slider_active');
+});
+slider.addEventListener('mousemove', e => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const SCROLL_SPEED = 1;
+  const walk = (x - startX) * SCROLL_SPEED;
+  slider.scrollLeft = scrollLeft - walk;
+});
