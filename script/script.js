c113c5cd616b71ffbeddc0edca84144b7a363f05
Eternatus <mr.drdioyt@gmail.com>
2023-04-27 00:16:24 +0500

    finishing 1 page: adding footer, maps and four blocks.

--- script/script.js
+++ script/script.js
@@ -1,21 +1,21 @@
-const slider = document.querySelector('.advantage_slider');
+const slider = document.querySelector('.reviews_slider');
 let isDown = false;
 let startX;
 let scrollLeft;
 
 slider.addEventListener('mousedown', e => {
   isDown = true;
-  slider.classList.add('advantage_slider_active');
+  slider.classList.add('reviews_slider_active');
   startX = e.pageX - slider.offsetLeft;
   scrollLeft = slider.scrollLeft;
 });
 slider.addEventListener('mouseleave', _ => {
   isDown = false;
-  slider.classList.remove('advantage_slider_active');
+  slider.classList.remove('reviews_slider_active');
 });
 slider.addEventListener('mouseup', _ => {
   isDown = false;
-  slider.classList.remove('advantage_slider_active');
+  slider.classList.remove('reviews_slider_active');
 });
 slider.addEventListener('mousemove', e => {
   if (!isDown) return;
