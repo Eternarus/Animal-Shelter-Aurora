0d945b9c9b7278cbef83b4ef9cfe1f00f7f510af
Eternatus <mr.drdioyt@gmail.com>
2023-06-13 17:13:05 +0500

    adaptive

--- script/slider.js
+++ script/slider.js
@@ -26,4 +26,28 @@ slider.addEventListener('mousemove', e => {
   slider.scrollLeft = scrollLeft - walk;
 });
 
+slider.addEventListener('touchdown', e => {
+  isDown = true;
+  slider.classList.add('reviews_slider_active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('toucnleave', _ => {
+  isDown = false;
+  slider.classList.remove('reviews_slider_active');
+});
+slider.addEventListener('toucnup', _ => {
+  isDown = false;
+  slider.classList.remove('reviews_slider_active');
+});
+slider.addEventListener('touchmove', e => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const SCROLL_SPEED = 1;
+  const walk = (x - startX) * SCROLL_SPEED;
+  slider.scrollLeft = scrollLeft - walk;
+});
+
+
 
